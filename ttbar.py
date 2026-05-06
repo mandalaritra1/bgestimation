b4dc79412e40305b0a72237f1436d7de92fcf1fa
Aritra Mandal <mandalaritra1@gmail.com>
2026-05-06 14:14:48 +0200

    Use robust 2024 FitDiagnostics options

--- ttbar.py
+++ ttbar.py
@@ -194,9 +194,9 @@ _rpf_options = {
 
 
     
-rmin = -6
+rmin = -1
 rmax = 6
-extra='--robustFit=1'
+extra='--robustFit=1 --setParameters r=0 --cminDefaultMinimizerStrategy 1 --cminFallbackAlgo Minuit2,Migrad,0:0.1 --cminFallbackAlgo Minuit2,Simplex,0:0.1'
 
 def _fix_integral_process_codes(card_path):
     '''
